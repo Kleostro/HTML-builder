16177153a402f11417f760e5a2206fd1dc1b4e2f
Kleostro <maxim_zabaluev@icloud.com>
2023-12-29 02:56:52 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathFile = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(pathFile);
+
+readStream.on('data', (chunk) => console.log(chunk.toString()));
